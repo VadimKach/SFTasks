7677448c9dff65a2b551274b0718cca20c3eb64f
VadimKachuryn <vadimkacurin71@gmail.com>
2022-07-04 18:04:11 +0300

    8.5 practical task

---
+++ src/main/java/m8t5/People.java
@@ -0,0 +1,21 @@
+package m8t5;
+
+public abstract class People {
+    String name;
+    int age;
+    String profession;
+
+
+    public People(String name, int age, String profession) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+
+
+    public abstract String getName();
+
+    public abstract int getAge();
+
+    public abstract String getProfession();
+}
